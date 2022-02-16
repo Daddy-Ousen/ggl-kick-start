9d2df5fee5a81a31cbb5085f459f0b951c109c41
Daddy-Ousen <rhasan228@gmail.com>
2022-02-16 19:53:13 +0600

    commit on main

--- candy.py
+++ candy.py
@@ -1,16 +1,16 @@
 def processcase(case_num):
-    # Read in the input for a single case
+    # Input for cases
     (num_candy_bags, num_kids) = tuple(map(int, input().split()))
     candy_counts = list(map(int, input().split()))
 
     # Calculate the amount of candy remaining
-    total_candy = 0
+    num_candy = 0
     for i in range(num_candy_bags):
-        total_candy += candy_counts[i]
+        num_candy += candy_counts[i]
     # Use module to calculate the remaining candy
-    amount_remaining = total_candy % num_kids
+    remaining_candy = num_candy % num_kids
 
-    print(f"Case #{case_num}: {amount_remaining}")
+    print(f"Case #{case_num}: {remaining_candy}")
 
 # Iterate through the each case
 num_cases = int(input())
